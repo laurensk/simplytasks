a123e05510bbfad0f6866234b3f0106f4e213dca
Laurens K <39766854+laurensk@users.noreply.github.com>
2021-01-31 22:44:39 +0100

    ios status bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,6 +11,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Simplytasks"),
+        brightness: Brightness.dark,
       ),
       body: Center(
         child: Text("Welcome to Simplytasks!"),
